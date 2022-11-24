6e0fbb15e3ee1d1e67f9d63898d2982456a4ab69
Arnav Jain <jainarnav@outlook.com>
2022-11-24 16:36:53 +0530

    major issue fixed

--- lib/screens/scoop.dart
+++ lib/screens/scoop.dart
@@ -23,7 +23,7 @@ class _ScoopStreamState extends State<ScoopStream> {
     return Scaffold(
       body: Container(
         width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height-1,
+        height: MediaQuery.of(context).size.height-0.1,
         child: FutureBuilder(
           future: NetworkSystem().contentBuilder(),
           builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
@@ -40,91 +40,103 @@ class _ScoopStreamState extends State<ScoopStream> {
                 child: ListView.builder(
                   scrollDirection: Axis.vertical,
                   controller: PageController(viewportFraction: 0.8),
-                  padding: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                  padding: EdgeInsets.symmetric(vertical: 0),
                   itemCount: snapshot.data!.length,
                   itemBuilder: (context, index) {
                     // print(snapshot.data![index].imgURL);
 
-                    return Container(
-                      padding: EdgeInsets.only(top: 70, left: 10, right: 10),
-                      height: MediaQuery.of(context).size.height,
-                      width: 100,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          fit: BoxFit.fill,
-                          image: NetworkImage(snapshot.data![index].imgURL)
-                        )
-                      ),
-                      child: BackdropFilter(
-                        filter: ImageFilter.blur(sigmaX: 180.0, sigmaY: 180.0),
-                        child: Column(
-                          children: [
-                            Container(
-                              padding: EdgeInsets.zero,
-                              margin: EdgeInsets.zero,
-                              width: (MediaQuery.of(context).size.width) - 30,
-                              height: (MediaQuery.of(context).size.width) - 50,
-                              // height: (MediaQuery.of(context).size.width) - 50,
-                              decoration: ShapeDecoration(
-                                image: DecorationImage(
-                                    image: NetworkImage(snapshot.data![index].imgURL),
-                                    fit: BoxFit.cover
-                                ),
-                                shadows: const [
-                                  BoxShadow(
-                                      color: Colors.black26,
-                                      blurRadius: 30.0,
-                                      spreadRadius: 5,
-                                      offset: Offset(
-                                          0,
-                                          6
-                                      )
-                                  )
-                                ],
-                                color: Colors.red.withOpacity(0.75),
-                                shape: SmoothRectangleBorder(
-                                  borderRadius: SmoothBorderRadius(
-                                    cornerRadius: 18,
-                                    cornerSmoothing: 0.9,
+                    return ClipRect(
+                      child: Container(
+                        padding: EdgeInsets.only(top: 70, left: 10, right: 10),
+                        height: MediaQuery.of(context).size.height,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(snapshot.data![index].imgURL)
+                          )
+                        ),
+                        child: BackdropFilter(
+                          // filter: ImageFilter.blur(sigmaY: 0, sigmaX: 0),
+                          // imageFilter: ImageFilter.blur(sigmaX: 200, sigmaY: 200, tileMode: TileMode.decal),
+                          filter: ImageFilter.blur(sigmaX: 500, sigmaY: 500),
+                          child: Column(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.zero,
+                                margin: EdgeInsets.zero,
+                                width: (MediaQuery.of(context).size.width) - 50,
+                                height: (MediaQuery.of(context).size.width) - 50,
+                                // height: (MediaQuery.of(context).size.width) - 50,
+                                decoration: ShapeDecoration(
+                                  image: DecorationImage(
+                                      image: NetworkImage(snapshot.data![index].imgURL),
+                                      fit: BoxFit.cover
+                                  ),
+                                  shadows: const [
+                                    BoxShadow(
+                                        color: Colors.black26,
+                                        blurRadius: 30.0,
+                                        spreadRadius: 5,
+                                        offset: Offset(
+                                            0,
+                                            6
+                                        )
+                                    )
+                                  ],
+                                  color: Colors.red.withOpacity(0.75),
+                                  shape: SmoothRectangleBorder(
+                                    borderRadius: SmoothBorderRadius(
+                                      cornerRadius: 18,
+                                      cornerSmoothing: 0.9,
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            SizedBox(height: 15),
-                            Text(snapshot.data![index].title,
-                              style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 20,
-                                letterSpacing: -0.5,
-                                color: Colors.black
-                              ),
-                            ),
-                            SizedBox(height: 7.5),
-                            SizedBox(
-                              height: 20,
-                              // width: 100,
-                              child: CupertinoButton(
-                                padding: EdgeInsets.zero,
-                                onPressed: () {},
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
+                              SizedBox(height: 15),
+                              Container(
+                                margin: EdgeInsets.only(top: 5),
+                                padding: EdgeInsets.symmetric(horizontal: 15),
+                                child: Column(
                                   children: [
-                                    Text(snapshot.data![index].source, style: TextStyle(color: Colors.black.withOpacity(0.3), fontWeight: FontWeight.w600),),
-                                    Icon(FeatherIcons.chevronRight, size: 19, color: Colors.black.withOpacity(0.3),)
+                                    Text(snapshot.data![index].title,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 20,
+                                          letterSpacing: -0.5,
+                                          color: Colors.white
+                                      ),
+                                    ),
+                                    SizedBox(height: 7.5),
+                                    SizedBox(
+                                      height: 20,
+                                      // width: 100,
+                                      child: CupertinoButton(
+                                        padding: EdgeInsets.zero,
+                                        onPressed: () {},
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.start,
+                                          children: [
+                                            Text(snapshot.data![index].source, style: TextStyle(color: Colors.white.withOpacity(0.5), fontWeight: FontWeight.w600),),
+                                            Icon(FeatherIcons.chevronRight, size: 19, color: Colors.white.withOpacity(0.3),)
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(height: 15),
+                                    Text(snapshot.data![index].content,
+                                      style: TextStyle(
+                                          color: Colors.white.withOpacity(0.7),
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w500,
+                                          letterSpacing: -0.4
+                                      ),
+                                    )
                                   ],
                                 ),
-                              ),
-                            ),
-                            SizedBox(height: 7.5),
-                            Text(snapshot.data![index].content,
-                              style: TextStyle(
-                                color: Colors.black.withOpacity(0.7),
-                                fontSize: 16,
-                                fontWeight: FontWeight.w500,
-                                letterSpacing: -0.4
-                              ),
-                            )
-                          ],
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     );
